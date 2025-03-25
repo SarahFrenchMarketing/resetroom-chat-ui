5ff503af152bcdc3d945061ae0e3b6ce0c5eb296
SarahFrenchMarketing <notioncaptureco@gmail.com>
2025-03-25 18:49:22 +0000

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,5 @@
-import Head from 'next/head';
-import ResetRoomChat from '../components/ResetRoomChat';
-
-export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>The Reset Room</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <main>
-        <ResetRoomChat />
-      </main>
-    </>
-  );
-}
+<Head>
+  <title>The Reset Room</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
+  <link href="https://fonts.googleapis.com/css2?family=Anton&family=Montserrat:wght@400;500&display=swap" rel="stylesheet" />
+</Head>
