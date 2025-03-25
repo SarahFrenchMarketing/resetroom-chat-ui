6df2e3f6160aabbd298a990d2231abe626f2e3e9
SarahFrenchMarketing <notioncaptureco@gmail.com>
2025-03-25 19:21:24 +0000

    Create tailwind.config.js

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}"
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        montserrat: ['Montserrat', 'sans-serif'],
+        anton: ['Anton', 'sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
